24c03dee6705cc3eb09eef29085fcec568f65ab9
stasdmitr <dmitrive2010>
2023-02-06 20:01:42 +0300

    OK

---
+++ NewsPaper/news/views.py
@@ -0,0 +1,50 @@
+from django.views.generic import (
+    ListView, DetailView, CreateView, UpdateView, DeleteView
+)
+from .models import Post
+from .filters import NewsFilter
+from django.urls import reverse_lazy
+from .forms import PostForm
+
+
+# Create your views here.
+class PostList(ListView):
+    model = Post
+    ordering = 'time_created'
+    template_name = 'news.html'
+    context_object_name = 'news'
+    paginate_by = 10
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        self.filterset = NewsFilter(self.request.GET, queryset)
+        return self.filterset.qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['filterset'] = self.filterset
+        return context
+
+
+class PostDetail(DetailView):
+    model = Post
+    template_name = 'post.html'
+    context_object_name = 'post'
+
+
+class PostCreate(CreateView):
+    form_class = PostForm
+    model = Post
+    template_name = 'post_edit.html'
+
+
+class PostUpdate(UpdateView):
+    form_class = PostForm
+    model = Post
+    template_name = 'post_edit.html'
+
+
+class PostDelete(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('post_list')
---
+++ venv/Lib/site-packages/allauth/socialaccount/providers/clever/urls.py
@@ -0,0 +1,5 @@
+from allauth.socialaccount.providers.oauth2.urls import default_urlpatterns
+
+from .provider import CleverProvider
+
+urlpatterns = default_urlpatterns(CleverProvider)
